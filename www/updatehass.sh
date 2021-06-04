9fc4d69a335fa9f4d4c4072043eb22414646204f
Greg de Lima <gregdelima@gmail.com>
2021-06-04 08:32:05 -0400

    Added Trash Day Calendars

--- www/updatehass.sh
+++ www/updatehass.sh
@@ -1,14 +1,13 @@
-#!/bin/bash
-
 #Move to docker directory
 cd /docker/homeassistant
 #Bring down the container with docker-compose
 docker-compose down
-#Grab the image ID into the variable. Docker images, search for home-assistant, print the 3rd column (the ID)
+#Grab the image ID into the variable. Docker images, search for home-assistant, print the 3rd column (the >
 img_dock=$(docker images | grep home-assistant  | awk '{ print $3 }')
 #Remove the image in the variable
 docker image rm $img_dock
+#remove image for zwavejs
+img_zwave=$(docker images | grep zwavejs  | awk '{ print $3 }')
+docker image rm $img_zwave
 #Bring container back up.
 docker-compose up -d
-
-exit
